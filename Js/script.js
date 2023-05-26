12206809c91dd977e6c4681029ecca4bb9bcb1d9
EsloverDev <edwin.grow.code@gmail.com>
2023-05-26 06:44:12 -0500

    Primer commit del proyecto GreenLight

---
+++ Js/script.js
@@ -0,0 +1,7 @@
+function greenlight(){
+    let nombre = "";
+    nombre = prompt("Cual es tu nombre?");
+    alert("bienvenido " + nombre );
+};
+e1 = document.querySelector(ul);
+e1.style.background = red
